fe62c2f257ba50057386de2efaae8a7d8aa1c14a
Badr JLIL <badr.jlil@outlook.com>
2025-03-03 10:28:09 +0000

    added sending mail while creating a test

--- BackEnd/TestService/src/main/java/com/example/demo/service/TestService.java
+++ BackEnd/TestService/src/main/java/com/example/demo/service/TestService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.api.CandidatClient;
 import com.example.demo.api.CompetenceClient;
+import com.example.demo.api.EmailingClient;
 import com.example.demo.api.LevelClient;
 import com.example.demo.api.QuestionClient;
 import com.example.demo.api.RoleClient;
@@ -22,111 +23,115 @@ import com.example.demo.model.*;
 
 @Service
 public class TestService {
-    @Autowired
-    private TestRepository testRepository;
-    @Autowired
-    private CandidatClient candidatClient;
-    @Autowired
-    private RoleClient roleClient;
-    @Autowired
-    private LevelClient levelClient;
-    @Autowired
-    private CompetenceClient competenceClient;
-    @Autowired
-    private QuestionClient questionClient;
+        @Autowired
+        private TestRepository testRepository;
+        @Autowired
+        private CandidatClient candidatClient;
+        @Autowired
+        private RoleClient roleClient;
+        @Autowired
+        private LevelClient levelClient;
+        @Autowired
+        private CompetenceClient competenceClient;
+        @Autowired
+        private QuestionClient questionClient;
+        @Autowired
+        private EmailingClient emailingClient;
 
+        public TestDTO createTest(TestDTO testDTO) {
+                Test test = new Test();
+                test.setName(testDTO.getName());
+                test.setRoleId(testDTO.getRole().getId());
+                test.setLevelId(testDTO.getLevel().getId());
 
-   public TestDTO createTest(TestDTO testDTO) {
-    Test test = new Test();
-    test.setName(testDTO.getName());
-    test.setRoleId(testDTO.getRole().getId());
-    test.setLevelId(testDTO.getLevel().getId());
+                List<Long> candidateIds = testDTO.getCandidats().stream()
+                                .map(c -> candidatClient.addCandidate(c).getId())
+                                .collect(Collectors.toList());
+                test.setCandidateIds(candidateIds);
 
-    List<Long> candidateIds = testDTO.getCandidats().stream()
-            .map(c -> candidatClient.addCandidate(c).getId())
-            .collect(Collectors.toList());
-    test.setCandidateIds(candidateIds);
+                List<Long> competenceIds = testDTO.getCompetences().stream()
+                                .map(c -> competenceClient.addCompetence(c).getId())
+                                .collect(Collectors.toList());
+                test.setCompetenceIds(competenceIds);
 
-    List<Long> competenceIds = testDTO.getCompetences().stream()
-            .map(c -> competenceClient.addCompetence(c).getId())
-            .collect(Collectors.toList());
-    test.setCompetenceIds(competenceIds);
+                // Ajouter seulement des IDs de questions existantes
+                List<Long> questionIds = testDTO.getQuestions().stream()
+                                .map(QuestionDTO::getId)
+                                .collect(Collectors.toList());
+                test.setQuestionIds(questionIds);
 
-    // Ajouter seulement des IDs de questions existantes
-    List<Long> questionIds = testDTO.getQuestions().stream()
-            .map(QuestionDTO::getId)
-            .collect(Collectors.toList());
-    test.setQuestionIds(questionIds);
+                Test savedTest = testRepository.save(test);
 
-    Test savedTest = testRepository.save(test);
-
-    return new TestDTO(
-            savedTest.getId(),
-            savedTest.getName(),
-            candidateIds.stream().map(candidatClient::getCandidateById).collect(Collectors.toList()),
-            roleClient.getRoleById(savedTest.getRoleId()),
-            levelClient.getLevelById(savedTest.getLevelId()),
-            competenceIds.stream().map(competenceClient::getCompetenceById).collect(Collectors.toList()),
-            questionIds.stream().map(questionClient::getQuestionById).collect(Collectors.toList())
-    );
-}
-
-public List<TestDTO> getAllTests() {
-    List<Test> tests = testRepository.findAll();
-
-    return tests.stream()
-            .map(test -> {
-                List<CandidatDTO> candidates = test.getCandidateIds().stream()
-                        .map(candidatClient::getCandidateById)
-                        .collect(Collectors.toList());
-
-                List<CompetenceDTO> competences = test.getCompetenceIds().stream()
-                        .map(competenceClient::getCompetenceById)
-                        .collect(Collectors.toList());
-
-                List<QuestionDTO> questions = test.getQuestionIds().stream()
-                        .map(questionClient::getQuestionById)
-                        .collect(Collectors.toList());
+                for (CandidatDTO candidat : testDTO.getCandidats()) {
+                        emailingClient.sendTestLink(candidat);
+                }
 
                 return new TestDTO(
-                        test.getId(),
-                        test.getName(),
-                        candidates,
-                        roleClient.getRoleById(test.getRoleId()),
-                        levelClient.getLevelById(test.getLevelId()),
-                        competences,
-                        questions
-                );
-            })
-            .collect(Collectors.toList());
-}
+                                savedTest.getId(),
+                                savedTest.getName(),
+                                candidateIds.stream().map(candidatClient::getCandidateById)
+                                                .collect(Collectors.toList()),
+                                roleClient.getRoleById(savedTest.getRoleId()),
+                                levelClient.getLevelById(savedTest.getLevelId()),
+                                competenceIds.stream().map(competenceClient::getCompetenceById)
+                                                .collect(Collectors.toList()),
+                                questionIds.stream().map(questionClient::getQuestionById).collect(Collectors.toList()));
+        }
 
-    public TestDTO getTestById(Long testId) {
-        return testRepository.findById(testId)
-                .map(test -> {
-                    List<CandidatDTO> candidates = test.getCandidateIds().stream()
-                            .map(candidatClient::getCandidateById)
-                            .collect(Collectors.toList());
-    
-                    List<CompetenceDTO> competences = test.getCompetenceIds().stream()
-                            .map(competenceClient::getCompetenceById)
-                            .collect(Collectors.toList());
-    
-                    List<QuestionDTO> questions = test.getQuestionIds().stream()
-                            .map(questionClient::getQuestionById)
-                            .collect(Collectors.toList());
-    
-                    return new TestDTO(
-                            test.getId(),
-                            test.getName(),
-                            candidates,
-                            roleClient.getRoleById(test.getRoleId()),
-                            levelClient.getLevelById(test.getLevelId()),
-                            competences,
-                            questions
-                    );
-                })
-                .orElse(null);
-    }
-    
+        public List<TestDTO> getAllTests() {
+                List<Test> tests = testRepository.findAll();
+
+                return tests.stream()
+                                .map(test -> {
+                                        List<CandidatDTO> candidates = test.getCandidateIds().stream()
+                                                        .map(candidatClient::getCandidateById)
+                                                        .collect(Collectors.toList());
+
+                                        List<CompetenceDTO> competences = test.getCompetenceIds().stream()
+                                                        .map(competenceClient::getCompetenceById)
+                                                        .collect(Collectors.toList());
+
+                                        List<QuestionDTO> questions = test.getQuestionIds().stream()
+                                                        .map(questionClient::getQuestionById)
+                                                        .collect(Collectors.toList());
+
+                                        return new TestDTO(
+                                                        test.getId(),
+                                                        test.getName(),
+                                                        candidates,
+                                                        roleClient.getRoleById(test.getRoleId()),
+                                                        levelClient.getLevelById(test.getLevelId()),
+                                                        competences,
+                                                        questions);
+                                })
+                                .collect(Collectors.toList());
+        }
+
+        public TestDTO getTestById(Long testId) {
+                return testRepository.findById(testId)
+                                .map(test -> {
+                                        List<CandidatDTO> candidates = test.getCandidateIds().stream()
+                                                        .map(candidatClient::getCandidateById)
+                                                        .collect(Collectors.toList());
+
+                                        List<CompetenceDTO> competences = test.getCompetenceIds().stream()
+                                                        .map(competenceClient::getCompetenceById)
+                                                        .collect(Collectors.toList());
+
+                                        List<QuestionDTO> questions = test.getQuestionIds().stream()
+                                                        .map(questionClient::getQuestionById)
+                                                        .collect(Collectors.toList());
+
+                                        return new TestDTO(
+                                                        test.getId(),
+                                                        test.getName(),
+                                                        candidates,
+                                                        roleClient.getRoleById(test.getRoleId()),
+                                                        levelClient.getLevelById(test.getLevelId()),
+                                                        competences,
+                                                        questions);
+                                })
+                                .orElse(null);
+        }
+
 }
